dca12931e3ccc3f447abb9e20024e10ed5bdd2d6
vovasap <rednotgreen2020@gmail.com>
2021-01-29 07:32:39 +0300

    workflow: update environment

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,64 @@
 const path = require('path');
-const HTMLWebpackPlugin = require('html-webpack-plugin');
+const { VueLoaderPlugin } = require('vue-loader');
+require('dotenv').config();
 
-module.exports = {
-  mode: 'development',
-  entry: './src/main.ts',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.ts/,
-        loader: 'ts-loader',
-        exclude: /node_modules/,
+module.exports = () => {
+  // eslint-disable-next-line no-console
+  console.log('NODE_ENV: ', process.env.NODE_ENV);
+  return {
+    mode: 'development',
+    entry: './src/main.js',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'bundle.js',
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src/'),
       },
-      {
-        test: /\.html/,
-        loader: 'html-loader',
-      },
+      extensions: ['.ts', '.tsx', '.js'],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.ts$/i,
+          loader: 'ts-loader',
+          options: {
+            appendTsSuffixTo: [/\.vue$/],
+          },
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.vue$/,
+          loader: 'vue-loader',
+        },
+        {
+          test: /\.js$/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
+          },
+          exclude: (file) => (
+            /node_modules/.test(file)
+          ),
+        },
+        {
+          test: /\.scss$/i,
+          use: ['style-loader', 'css-loader', 'sass-loader'],
+        },
+      ],
+    },
+    plugins: [
+      new VueLoaderPlugin(),
     ],
-  },
-  plugins: [
-    new HTMLWebpackPlugin(),
-  ],
-  devServer: {
-    port: 8080,
-    open: true,
-  },
-
+    optimization: {
+      minimize: true,
+    },
+    devServer: {
+      inline: true,
+      hot: true,
+    },
+  };
 };
